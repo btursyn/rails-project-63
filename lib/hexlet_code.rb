b358a8ba5b8758abda0befe52611c808d993d455
Bauyrzhan <tursynbauyrzhan@gmail.com>
2023-11-29 23:58:25 +0600

    fix linter

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "hexlet_code/version"
-
+# entry level module for gem
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
   autoload(:Tag, "./lib/hexlet_code/tag.rb")
   HexletCode::Tag.build("img", src: "path/to/image", a: "path/to/image")
   HexletCode::Tag.build("input", type: "submit", value: "Save")
