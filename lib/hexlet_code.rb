25ff0b7a8158c3827b4e4b39bf28f5d8418b84be
Bauyrzhan <tursynbauyrzhan@gmail.com>
2023-12-01 01:10:17 +0600

    add form_for method

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -7,4 +7,8 @@ module HexletCode
   autoload(:Tag, "./lib/hexlet_code/tag.rb")
   HexletCode::Tag.build("img", src: "path/to/image", a: "path/to/image")
   HexletCode::Tag.build("input", type: "submit", value: "Save")
+  def self.form_for(object, options = {})
+    empty_form = "<form action=\"#\" method=\"post\"></form>"
+    options.include?(:url) ? empty_form.sub("#", options[:url]) : empty_form
+  end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -3,11 +3,25 @@
 require "test_helper"
 
 class TestHexletCode < Minitest::Test
+  User = Struct.new(:id, :name, :job)
+
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
 
-  def test_it_does_something_useful
-    assert true
+  def setup
+    @user = User.new
+  end
+
+  def test_empty_form_with_empty_block
+    form = HexletCode.form_for(@user) do |f|
+    end
+    assert_equal form, "<form action=\"#\" method=\"post\"></form>"
+  end
+
+  def test_form_with_url_with_empty_block
+    form = HexletCode.form_for(@user, url: "/users") do |f|
+    end
+    assert_equal form, "<form action=\"/users\" method=\"post\"></form>"
   end
 end
