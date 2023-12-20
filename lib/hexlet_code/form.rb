feb1e53549e4db5f069eea16e991751fb8f80fb9
Bauyrzhan <tursynbauyrzhan@gmail.com>
2023-12-21 03:40:49 +0600

    make Form singleton methods in class << self

--- lib/hexlet_code/form.rb
+++ lib/hexlet_code/form.rb
@@ -3,29 +3,31 @@
 module HexletCode
   # module for generating html forms
   module Form
-    def self.build(attributes = {}, &)
-      attributes = get_form_attributes attributes
+    class << self
+      def build(attributes = {}, &)
+        attributes = get_form_attributes attributes
 
-      if block_given?
-        Tag.build('form', attributes, &)
-      else
-        Tag.build('form', attributes) { '' }
+        if block_given?
+          Tag.build('form', attributes, &)
+        else
+          Tag.build('form', attributes) { '' }
+        end
+      end
+
+      private
+
+      def get_form_attributes(attributes)
+        if attributes.key?(:url)
+          attributes[:action] = attributes[:url]
+          attributes.delete(:url)
+        end
+
+        default_form_attributes = { action: '#', method: 'post' }
+        (default_form_attributes.keys - attributes.keys).each do |item|
+          attributes[item] = default_form_attributes[item]
+        end
+        attributes
       end
     end
-
-    def self.get_form_attributes(attributes)
-      if attributes.key?(:url)
-        attributes[:action] = attributes[:url]
-        attributes.delete(:url)
-      end
-
-      default_form_attributes = { action: '#', method: 'post' }
-      (default_form_attributes.keys - attributes.keys).each do |item|
-        attributes[item] = default_form_attributes[item]
-      end
-      attributes
-    end
-
-    private_class_method :get_form_attributes
   end
 end
