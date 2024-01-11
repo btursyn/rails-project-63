e49bcd34c651a0fb7e1588b86056aba5c3b0de1f
Bauyrzhan <tursynbauyrzhan@gmail.com>
2024-01-11 09:53:41 +0600

    requiere neccasary gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-require 'bundler/setup'
-Bundler.require
 require 'hexlet_code'
 require 'minitest/autorun'
+require 'minitest-power_assert'
 
 def load_html_fixture(file_name)
   html = File.read "#{File.dirname(__FILE__)}/fixtures/#{file_name}"
